ab007c6dca7784b42b2ba06e7172fab894dfa62f
Jeffrey <jeffrey.shively@hashicorp.com>
2022-04-28 11:03:27 -0400

    Changing the website

--- web.sh
+++ web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum install httpd -y
-echo 'Built using Packer and a shell script' >> index.html
+echo 'Built using HCP Packer and a shell script (production channel)' >> index.html
 sudo mv index.html /var/www/html/index.html
 sudo systemctl enable httpd
 sudo systemctl start httpd
