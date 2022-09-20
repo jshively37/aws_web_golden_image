c4d53a6fab41ac605719c382fa5cfa6e10e1fe09
Jeffrey <jeffrey.shively@hashicorp.com>
2022-09-20 13:31:11 -0400

    Updating image

--- web.sh
+++ web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum install httpd -y
-echo 'Built using HCP Packer and a shell script (production channel v4)' >> index.html
+echo 'Built using HCP Packer and a shell script (production channel updated)' >> index.html
 sudo mv index.html /var/www/html/index.html
 sudo systemctl enable httpd
 sudo systemctl start httpd
