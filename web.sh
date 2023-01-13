388b49aa060ab54cc81257e5572dcd3043fa87b6
Jeffrey <jeffrey.shively@hashicorp.com>
2023-01-13 09:46:00 -0500

    Update webpage

--- web.sh
+++ web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum install httpd -y
-echo 'Favorite cat this week: Wrigley' >> index.html
+echo 'Built using HCP Packer and a shell script (production channel updated) - ' >> index.html
 sudo mv index.html /var/www/html/index.html
 sudo systemctl enable httpd
 sudo systemctl start httpd
